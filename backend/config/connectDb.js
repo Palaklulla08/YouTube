38083e9109c6ff26aa38c25f55f41e9fe63f8908
Palaklulla_08 <palaklulla54@gmail.com>
2025-12-09 01:21:47 +0530

    created the file connectdb in config folder to connect to mongodb by its url stored in .env file

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -1,7 +1,9 @@
+// importing mongoose module
 import mongoose from "mongoose";
-
+//  function to connect to mongodb database
 const connectDb = async () => {
     try {
+        // MONGODB_URL is the mongo atlas connection string stored in .env file
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("DB Connected")
     } catch (error) {
