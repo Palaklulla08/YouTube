e77ae34589102097562fce170dbebce5b84f15af
Palaklulla_08 <palaklulla54@gmail.com>
2025-12-09 01:19:58 +0530

    created the cloudinary.js file in config folder with function uploadoncloudinary which will config the cloudinary and give a url that is accessible all over the internet

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,34 +1,8 @@
-// import { v2 as cloudinary } from 'cloudinary'
-
-// import fs from "fs"
-
-
-// const uploadOnCloudinary = async (filePath) => {
-//     cloudinary.config({ 
-//   cloud_name: process.env.CLOUDINARY_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY, 
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
-
-// try {
-//     if(!filePath){
-//         return null
-//     }
-//     const uploadResult = await cloudinary.uploader.upload(filePath,{resource_type:'auto'})
-//     fs.unlinkSync(filePath)
-//     return uploadResult.secure_url
-
-// } catch (error) {
-//     fs.unlinkSync(filePath)
-//     console.log(error)
-// }
-// }
-
-// export default uploadOnCloudinary
-
+// importing cloudinary and fs modules
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
 
+// creatinf function to upload file to cloudinary
 const uploadOnCloudinary = async (filePath) => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -37,9 +11,11 @@ const uploadOnCloudinary = async (filePath) => {
   });
 
   try {
+    // if file path is not provided
     if (!filePath) return null;
-
+    // uploading file to cloudinary
     const uploadResult = await cloudinary.uploader.upload(filePath, {
+        // resouretype is auto weather it is image or video
       resource_type: "auto",
     });
 
@@ -62,5 +38,5 @@ const uploadOnCloudinary = async (filePath) => {
     return null;
   }
 };
-
+// exporting default uploadOnCloudinary;
 export default uploadOnCloudinary;
