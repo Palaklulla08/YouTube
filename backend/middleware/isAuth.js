0a777243af1fa6e7bb50583ed278d5c119512451
Palaklulla_08 <palaklulla54@gmail.com>
2025-12-09 21:23:56 +0530

    is auth middleware for user authentication

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,15 +1,21 @@
+// importing jwt 
 import jwt from "jsonwebtoken"
 
+// function isauth set token from req.cookies 
 const isAuth = async (req,res,next) => {
     try {
         let {token} = req.cookies
+        // if token not received user doest have token returns
     if(!token){
         return res.status(400).json({message:"user does't have token"})
     }
+    // if received then token is verified 
     let verifyToken = await jwt.verify(token,process.env.JWT_SECRET)
+    // if the verification not passes  then return invalid user
     if(!verifyToken){
         return res.status(400).json({message:"user does't have valid token"})
     }
+    // if verified then setting req.user as verifytoken.userid and moving to next middleware
     req.userId = verifyToken.userId
     next()
 
@@ -20,3 +26,4 @@ const isAuth = async (req,res,next) => {
 }
 
 export default isAuth
+// -----------------------------------------------------------------------------
