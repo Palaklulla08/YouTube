28e4b6b200684eeb501698e9c26aa53a42407000
Palaklulla08 <palaklulla54@gmail.com>
2026-03-01 23:08:16 +0530

    main auth axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
+import axios from "axios"
+
+axios.defaults.baseURL = "https://youtube-oodn.onrender.com"
+axios.defaults.withCredentials = true
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
